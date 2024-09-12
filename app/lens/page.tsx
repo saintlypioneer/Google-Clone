2449e97dcceca35a492416f3ca833550cca17754
SaintlyPioneer <aggrawalankitsra@gmail.com>
2024-09-12 16:52:10 +0530

    complete template for image crop handler during image search

--- app/lens/page.tsx
+++ app/lens/page.tsx
@@ -1,9 +1,14 @@
 import GoogleLensNavigation from "../components/GoogleLensNavigation";
+import ImageSearchImageHandler from "../components/ImageSearchImageHandler";
 
 export default function Lens(){
     return (
-        <div>
+        <div className="h-screen flex flex-col overflow-y-clip">
             <GoogleLensNavigation />
+            <div className="flex-1 grid grid-cols-2 h-full overflow-y-clip">
+                <ImageSearchImageHandler />
+                <div className="bg-white">RIGHT</div>
+            </div>
         </div>
     );
 }
