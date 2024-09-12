e0467356cbf11a880930135290576e7df92fd78b
SaintlyPioneer <aggrawalankitsra@gmail.com>
2024-09-12 11:20:23 +0530

    force dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SimpleSearchSection from "./components/SimpleSearchSection";
 
 export default function Home() {
   return (
-    <div className="dark:bg-[#202124] dark:text-[#e8e8e8] text-sm font-[550]  min-h-screen w-screen flex flex-col">
+    <div className="dark dark:bg-[#202124] dark:text-[#e8e8e8] text-sm font-[550]  min-h-screen w-screen flex flex-col">
       <SimpleHeader />
       <main className="flex-1 flex flex-col">
         <SimpleSearchSection />
